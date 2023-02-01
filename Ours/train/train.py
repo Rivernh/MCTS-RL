ae5b9179a3e4186c97381fe790c14aa78eb7d92f
liuhanyin <15399632612@163.com>
2023-02-01 10:40:58 +0800

    update

--- Ours/train/train.py
+++ Ours/train/train.py
@@ -10,6 +10,10 @@ import random
 import numpy as np
 from collections import defaultdict, deque
 import sys
+import cv2
+import glob
+import os
+import json
 """
 sys.path.append(r'/home/ylh/MCTS-Carla/scripts/Ours')
 from Module.mcts_withnet import MCTSPlayer
@@ -17,6 +21,50 @@ from Module.policy import PolicyNet as PolicyValueNet# Pytorch
 """
 from ..Module.policy import PolicyValueNet
 from ..Module.mcts_withnet import MCTSPlayer
+
+
+class generator():
+    def __init__(self, filepath="/home/ylh/MCTS-RL/Ours/data", batch_size=16):  # 形参用到的，路径+batchsize+是否数据增强
+        self.base_dir = filepath
+        self.image_names = os.listdir(filepath + '/img')  # 所有图片名称
+        np.random.shuffle(self.image_names)
+        self.index = 0  # 注意位置
+        self.count = len(self.image_names)
+        self.batch_size = batch_size
+
+    def next(self, batch_size=16):
+        images = []
+        labels = []
+
+        while True:
+            self.index = self.index % self.count  # 1%300008=1
+            if self.index == 0:  # 跑完一个轮回，重新打乱
+                np.random.shuffle(self.image_names)
+            image_path = self.image_names[self.index]
+            self.index += 1
+            image = cv2.imread(image_path)
+            if image is None:
+                print("\n[WARRING]: 读取图片 '{}' 失败".format(image_path))
+                continue
+            images.append(image)  # list中存放各个图片的np格式
+
+            state_path = image_path.split('img/')[0] + 'state/' + image_path.split('img/')[1].split('.')[0] + '.json'
+            # 读取.json格式文件的内容
+            with open(state_path, 'r+') as file:
+                content = file.read()
+
+            label = json.loads(content)
+            labels.append(label)
+
+            if len(images) == batch_size:
+                break  # 一次batchsize完毕，推出循环
+
+        images = np.asarray(images, dtype=np.uint8)  # 将list格式的images转为n h w c用于data_aug_sequential输入
+
+        return images / 255, labels
+
+    def __next__(self):
+        return self.next(self.batch_size)
 
 class TrainPipeline():
     def __init__(self, init_model=None):
@@ -55,22 +103,25 @@ class TrainPipeline():
                                       n_playout=self.n_playout,
                                       is_selfplay=1)
 
-    def get_minibatch(self, dir = './'):
+    def get_minibatch(self, file_path):
         batch_size = self.batch_size
-        return
+        my_generator = generator(file_path, batch_size)
+        return my_generator.next()
 
     def policy_update(self):
         """update the policy-value net"""
         mini_batch = self.get_minibatch()
-        state_batch = [data[0] for data in mini_batch]
-        mcts_probs_batch = [data[1] for data in mini_batch]
+        img_batch = [data[0] for data in mini_batch]
+        d_batch = [data[1] for data in mini_batch]
+        state_batch = [data['state'] for data in d_batch]
+        mcts_probs_batch = [data['prob'] for data in d_batch]
         winner_batch = [data[2] for data in mini_batch]
         old_probs, old_v = self.policy_value_net.policy_value(state_batch)
         for i in range(self.epochs):
             loss, entropy = self.policy_value_net.train_step(
+                    img_batch,
                     state_batch,
                     mcts_probs_batch,
-                    winner_batch,
                     self.learn_rate*self.lr_multiplier)
             new_probs, new_v = self.policy_value_net.policy_value(state_batch)
             kl = np.mean(np.sum(old_probs * (
@@ -84,13 +135,6 @@ class TrainPipeline():
             self.lr_multiplier /= 1.5
         elif kl < self.kl_targ / 2 and self.lr_multiplier < 10:
             self.lr_multiplier *= 1.5
-
-        explained_var_old = (1 -
-                             np.var(np.array(winner_batch) - old_v.flatten()) /
-                             np.var(np.array(winner_batch)))
-        explained_var_new = (1 -
-                             np.var(np.array(winner_batch) - new_v.flatten()) /
-                             np.var(np.array(winner_batch)))
         print(("kl:{:.5f},"
                "lr_multiplier:{:.3f},"
                "loss:{},"
@@ -100,9 +144,7 @@ class TrainPipeline():
                ).format(kl,
                         self.lr_multiplier,
                         loss,
-                        entropy,
-                        explained_var_old,
-                        explained_var_new))
+                        entropy))
         return loss, entropy
 
     def run(self):
